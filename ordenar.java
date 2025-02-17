c4c6607e49f0190abb24e1d5cbf8fddf197c94b7
Cauã Henrique Ramos Rezende <140010145+Cauahenrique08@users.noreply.github.com>
2025-02-16 21:53:24 -0500

    Update ordenar.java

--- ordenar.java
+++ ordenar.java
@@ -11,11 +11,11 @@ public class ordenar {
 	  public int  fatorial (int  N) 
 	  { 
 		
-		 //condição de parada 
+		 //condição de parada, pois caso seja N == 1 não necessita multiplicar por 1 pois dará o mesmo valor.
 		  if (N == 1)
 		  {return 1;}
 		  else 
-		  {  return N = N * fatorial(N-1);
+		  {  return N = N * fatorial(N-1); // retorna o resultado da recursividade
 			  
 		  }
 		      
